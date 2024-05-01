f12b5e20ed5a7355266600420116fb43848d601b
Umesh Itukade <umesh.itukade@gunadhyasoft.com>
2024-05-01 12:30:20 +0530

    basic project template

---
+++ vite-project/src/tests/sum.test.js
@@ -0,0 +1,9 @@
+// sum.test.js
+import { describe, expect, it, test } from "vitest";
+import { sum } from "./sum";
+import { mul } from "./mul";
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(mul(4,2)).toBe(8);
+});
+
